9e87b5e4a61455cf4a5f8d0fe7a4ba98924f2c3a
bryanlin <bryan.boyanlin@gmail.com>
2017-03-21 22:00:55 -0700

    Add a few more files

---
+++ configs/README.md
@@ -0,0 +1,7 @@
+## mp.js
+```
+module.exports = {
+  appID: 'the app id fetched from mp.wechat',
+	appSecret: 'app Secret'
+}
+```
---
+++ polyfill.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * Require this module to polyfill some handy features to js native objects.
+ */
+
+if (!String.prototype.format) {
+ String.prototype.format = function() {
+   var args = arguments;
+   return this.replace(/{(\d+)}/g, (match, number) => {
+     return typeof args[number] != 'undefined'
+       ? args[number]
+       : match;
+   });
+ };
+}
